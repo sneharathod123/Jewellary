c3a5d33008e167ca38fc4befd5468e581b942f67
sneha <sneha.bvminfotech@gmail.com>
2024-07-08 11:26:20 +0530

    Bracelet collection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import CollectionPage from "./components/CollectionPage";
 import NeckLessCollectionH from "./components/NeckLessCollectionH";
+import GotoTop from "./components/GotoTop";
+import Bracelet from "./components/Bracelet";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<CollectionPage />} />
         <Route path="/Neckless" element={<NeckLessCollectionH />} />
+        <Route path="/Bracelet" element={<Bracelet />} />
       </Routes>
+      <GotoTop/>
     </BrowserRouter>
   );
 }
